01f93c1ef135077120ff5b070da1bdf79f1e601f
johnnync13 <johnnynuca14@gmail.com>
2019-06-13 10:14:49 +0200

    examen año pasado

---
+++ resum pract.sql
@@ -0,0 +1,5 @@
+SELECT: especifica les columnes que volem recuperar i mostrar en el resultat.
+FROM: especifica les taules que utilitzarem en la consulta.
+WHERE: filtra les files de la consulta segons una condició.
+GROUP BY: agrupa les files del resultat que tinguin el mateix valor a una columna.
+HAVING: filtra els resultats segons una condició.
